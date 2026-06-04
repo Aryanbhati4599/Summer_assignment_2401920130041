b8fe8159fd8135f02d9e260bfe0f1bc96ad3c4bf
Aryanbhati4599 <aryanbhatipalla@gmail.com>
2026-06-04 22:37:15 +0530

    Create Diagonalsum.java

---
+++ Week 1/Day 4/Diagonalsum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int n = mat.length;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += mat[i][i];           // Primary diagonal
+            sum += mat[i][n - 1 - i];   // Secondary diagonal
+        }
+
+        if (n % 2 == 1) {
+            sum -= mat[n / 2][n / 2];   // Remove duplicate center element
+        }
+
+        return sum;
+    }
+}
